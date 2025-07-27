34e0a267aedbbeba0c5cdc867f3f9504b496988f
nishant1479 <nishantdubey147@gmail.com>
2025-07-27 22:42:32 +0530

    feat: PS product service

--- product-service/internal/service/service.go
+++ product-service/internal/service/service.go
@@ -1,1 +1,29 @@
 package service
+
+import (
+	"context"
+	"nishant/internal/data"
+	"nishant/internal/models"
+)
+
+type ProductService struct {
+	productStore *data.ProductStore
+}
+
+func NewProductService(productStore *data.ProductStore) *ProductService {
+	return &ProductService{
+		productStore: productStore,
+	}
+}
+
+func (s *ProductService) GetAllProduct(ctx context.Context) ([]*model.Product, error) {
+	products, err := s.productStore.GetAllProduct(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return products, nil
+}
+
+func (s *ProductService) GetProductById(ctx context.Context, id string) (*model.Product, error) {
+	return s.productStore.GetProductById(ctx, id)
+}
