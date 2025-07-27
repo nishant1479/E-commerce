3e544d0b723e4a3e88af55ab52d4dca47a9bb3b4
nishant1479 <nishantdubey147@gmail.com>
2025-07-27 23:43:26 +0530

    feat: US services

--- user-service/internal/services/sevice.go
+++ user-service/internal/services/sevice.go
@@ -1,1 +1,50 @@
-package services
+package service
+
+import (
+	"context"
+	"errors"
+	"nishant/internal/data"
+	"nishant/internal/models"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type UserService struct {
+	userStore *data.UserStore
+}
+
+func NewUserService(userStore *data.UserStore) *UserService {
+	return &UserService{
+		userStore: userStore,
+	}
+}
+
+func (s *UserService) GetUser(ctx context.Context, id string) (*models.User, error) {
+	return s.userStore.GetUser(ctx, id)
+}
+func (s *UserService) CreateUser(ctx context.Context, user *models.User) (*models.User, error) {
+	// check if the user is registered
+	exist, err := s.userStore.GetUserByEmail(ctx, user.Email)
+	if err != nil {
+		if err != mongo.ErrNoDocuments {
+			return nil, err
+		}
+	} else if exist != nil {
+		return nil, errors.New("user already registered")
+	}
+
+	// have to hash the password
+
+	return s.userStore.CreateUser(ctx, user)
+}
+
+func (s *UserService) Login(ctx context.Context, email, password string) (string, error) {
+	user, err := s.userStore.GetUserByEmail(ctx, email)
+	if err != nil {
+		return "", err
+	}
+	// check if password is correct
+
+
+	//have to generate jwt token
+	return "",nil
+}
